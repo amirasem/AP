a5e9d5895ac91726fb3f5d807dbb08fa2775893d
amirasem <amiryousefi98@gmail.com>
2017-05-24 11:27:12 -0700

    Animal

---
+++ animals/properties/Live.java
@@ -0,0 +1,11 @@
+package game.animals.properties;
+
+import game.animals.properties.values.LivingPropertyValueType;
+
+/**
+ * Created by ahay on 4/19/17.
+ * GIBILI GIBILI
+ */
+public interface Live {
+    public void change(LivingPropertyType type, LivingPropertyValueType valueType, int value);
+}
---
+++ animals/properties/values/LivingPropertyValueType.java
@@ -0,0 +1,11 @@
+package game.animals.properties.values;
+
+/**
+ * Created by ahay on 4/17/17.
+ * GIBILI GIBILI
+ */
+public enum LivingPropertyValueType {
+    MAXIMUM, CURRENT, REFILL_RATE, CONSUMPTION_RATE;
+
+
+}
